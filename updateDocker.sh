32fa228e68df399bd199c0af284fa313001d4c81
xu3cl40122 <xu3cl40122>
2021-06-22 13:17:15 +0000

    Fix: sh

--- updateDocker.sh
+++
@@ -1,5 +0,0 @@
-cd /home/ec2-user/projects/go-court-service/
-# 取得新的 image
-docker-compose -f docker-compose.prod.yml pull
-docker-compose -f docker-compose.prod.yml up -d --remove-orphans
-docker image prune -f
